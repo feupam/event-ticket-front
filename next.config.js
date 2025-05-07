5bfb26f1a00ba16258e3334b4a71d4ba11279878
pv-luizalmeida <125695179+pv-luizalmeida@users.noreply.github.com>
2025-05-07 17:57:14 -0300

    Adiciona otimizações de webpack e habilita minificação com SWC nas configurações do Next.js. Remove arquivos de configuração do Bolt que não são mais necessários.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
   env: {
     VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
     VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -43,6 +44,42 @@ const nextConfig = {
         permanent: true,
       }
     ]
+  },
+  webpack: (config, { isServer, dev }) => {
+    // Ativar otimizações do webpack mesmo em modo de desenvolvimento
+    config.optimization.minimize = true;
+    
+    // Dividir o bundle em partes menores (chunks)
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      maxInitialRequests: 25,
+      minSize: 20000,
+      maxSize: 1000000, // 1MB por arquivo
+      cacheGroups: {
+        // Separar cada biblioteca grande em seu próprio chunk
+        firebase: {
+          test: /[\\/]node_modules[\\/](firebase|@firebase)[\\/]/,
+          name: 'firebase',
+          priority: 20,
+        },
+        framework: {
+          test: /[\\/]node_modules[\\/](react|react-dom|next)[\\/]/,
+          name: 'framework',
+          priority: 10,
+        },
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // Obter o nome do pacote para dar nomes significativos aos chunks
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+          priority: 5,
+        }
+      }
+    };
+
+    return config;
   },
 }
 
